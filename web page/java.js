768a551ef3fd14acd445d19be9137ecec7b18601
Rahul-college <rahulsingh79p@gmail.com>
2024-03-04 23:02:36 +0530

    modified

---
+++ web page/java.js
@@ -0,0 +1,14 @@
+
+<script>
+    function take value()
+    
+        Var n1 = document.getElementByid("n1").value;
+        var n2 = document.getElementByid("n2").value;
+        var n3= document.getElementByid("n3").value;
+
+        document.write("name="+name);
+        document.write("name="+name);
+        document.write("name="+name);
+    
+</script>
+
